5135a91f4451ab4c111f0ec89ba92d6e35ca49ec
meshal <meshal.aneze@gmail.com>
2019-04-21 00:51:07 +0300

    add link and color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import '../App.css';
 
 class Home extends Component{
@@ -8,17 +9,17 @@ class Home extends Component{
                 <div className="content-cover justify-content-center container-fluid text-center d-flex p-3 mx-auto flex-column">
                     <main role='main' className='container center p-3 m-5'>
                             
-                            <div class="icon-food fa-2x mb-5">
+                            <div class="icon-food big-icon fa-2x mb-5">
                             </div>
                             <h1 className='lead'>
                                 وين ناكل؟
                             </h1>
                             <div class="btn-group" role="group" aria-label="Basic example">
                             <button type="button" class="btn btn1 mr-1"><span className='icon-wrench fa-4x'></span></button>
+                            <Link to="/resturant">
                             <button type="button" class="btn">اقترح</button>
+                            </Link>
                             </div>
-                        
-                        
                        
                     </main>   
                 </div>
